a765ae7aa14b290bbbfaf1021f1eee3616d365b3
HaesungKang <koh1260@naver.com>
2022-12-29 10:51:28 +0900

    detail html

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from .models import HelloWorld
 from django.urls import reverse, reverse_lazy
@@ -33,3 +33,7 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    template_name = 'accountapp/detail.html'
