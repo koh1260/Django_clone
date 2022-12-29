763d2eb0bfd5560f98110ce7b1ce75cc67555e10
HaesungKang <koh1260@naver.com>
2022-12-30 05:44:16 +0900

    delete

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, DeleteView
 
 from .forms import AccountUpdateForm
 from .models import HelloWorld
@@ -48,3 +48,8 @@ class AccountUpdateView(CreateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
